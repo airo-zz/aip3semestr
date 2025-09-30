d665f1133af19eadafc0357e84ca7d697353cc88
airo <admn@airo.local>
2025-09-30 12:27:16 +0300

    choto8

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,10 @@
 "use strict"; // строгий режим
-let str1 = 'xxx';
-let str2 = 'yyy';
-let txt = `aaa ${str1} bbb ${str2} ccc`;
+let t = true;
+let f = false;
+let n = null;
+let u; // undefined
+
+alert('true: ' + t);
+alert('false: ' + f);
+alert('null: ' + n);
+alert('undefined: ' + u);
