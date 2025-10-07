2b9db0a8fc01c8c915d6c948cd312523e236e1f3
airo <admn@airo.local>
2025-10-07 11:35:33 +0300

    choto13

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,5 +1,11 @@
 "use strict"; // строгий режим
 
-let a = +'2';
-let b = +'3';
-alert(a + b);
+let a = '5px'
+let b = '6px'
+let c = parseInt(a) + parseInt(b)
+alert(c)
+
+let d = '5.5px'
+let e = '6.25px'
+let f = parseFloat(d) + parseFloat(e) + 'px'
+alert(f)
