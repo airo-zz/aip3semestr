9a092b4f3fce0ab2edc1fd4e008be5da4f2d08b1
airo <admn@airo.local>
2025-10-07 12:19:18 +0300

    choto17

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,5 +1,5 @@
 "use strict"; // строгий режим
 
 let str = 'abcde';
-let reversed = str[4] + str[3] + str[2] + str[1] + str[0];
-alert(reversed); // 'edcba'
+let num = 2; // номер символа (индекс)
+alert(str[num]);
