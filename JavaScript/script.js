fea2f6aa33a90d78b1f22a3413dc5dcb4da1c427
airo <admn@airo.local>
2025-10-07 11:14:08 +0300

    choto10

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,8 +1,9 @@
 "use strict"; // строгий режим
 
-alert(eee);
+let num = 123;
+console.log(num);
 
-console.log(123);
-console.log('123');
-console.log(true);
-console.log(null);
+let num1 = 123;
+let num2 = 456;
+let num3 = 789;
+console.log(num1, num2, num3);
