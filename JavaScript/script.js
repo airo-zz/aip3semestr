53dee002096ad1e79ae310fdc99e50e4627db31d
Emir Dzhalalov <dzalalovemran@gmail.com>
2025-10-22 17:13:26 +0300

    v64

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,20 +1,8 @@
 "use strict";
 
 
-let array1 = [1, 2, 3];
-console.log(array1[0]);
-console.log(array1[1]);
-console.log(array1[2]);
+let arr1 = [5, 'abc', true, null, 12];
+console.log(arr1.length);
 
-let arr = [1, 2, 3];
-console.log(arr[0]);
-console.log(arr[1]);
-console.log(arr[2]);
-
-let arr2 = [1, 2, 3];
-let sum = arr2[0] + arr2[1] + arr2[2];
-console.log(sum);
-
-let arr3 = ['a', 'b', 'c', 'd'];
-let str = arr3[0] + '+' + arr3[1] + '+' + arr3[2] + '+' + arr3[3];
-console.log(str);
+let arr2 = ['apple', 42, false, 3.14, 'end'];
+console.log(arr2[arr2.length - 1]);
