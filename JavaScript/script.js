fdbc5f31fcca3e76227f70264018f87c203a2890
Emir Dzhalalov <dzalalovemran@gmail.com>
2025-10-22 17:17:00 +0300

    v67

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,11 +1,7 @@
 "use strict";
 
-let numbers = [1, 2, 3, 4, 5];
-
-numbers[0] += 3;
-numbers[1] += 3;
-numbers[2] += 3;
-numbers[3] += 3;
-numbers[4] += 3;
-
-console.log(numbers); 
+let arr = [1, 2, 3];
+arr[0]++;
+arr[1]++;
+arr[2]++;
+console.log(arr);
