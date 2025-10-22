fe4817e3939a53033df117348040d0f506cf9ccb
Emir Dzhalalov <dzalalovemran@gmail.com>
2025-10-22 16:53:11 +0300

    v58

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,3 @@
 "use strict";
 
-let sideA = parseFloat(prompt("Введите длину первой стороны прямоугольника:"));
-let sideB = parseFloat(prompt("Введите длину второй стороны прямоугольника:"));
-let perimeter = 2 * (sideA + sideB);
-alert("Периметр прямоугольника: " + perimeter);
+document.write('<i>Hello</i>')
