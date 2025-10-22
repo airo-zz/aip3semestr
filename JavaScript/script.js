0181489a33ef2832ed26e7b4cd3d6da77b0e5269
Emir Dzhalalov <dzalalovemran@gmail.com>
2025-10-22 17:07:28 +0300

    v61

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,7 +1,30 @@
 "use strict";
 
 
-let num1 = 1;
-let num2 = 2;
-let sum = Number(num1) + Number(num2);
-document.write('сумма: ' + sum);
+let radius = 5;
+let areaCircle = Math.PI * radius * radius;
+console.log(areaCircle);
+
+let sideSquare = 4;
+let areaSquare = sideSquare * sideSquare;
+console.log(areaSquare);
+
+let sideA = 3;
+let sideB = 6;
+let areaRectangle = sideA * sideB;
+console.log(areaRectangle);
+
+sideA = 3;
+sideB = 6;
+let perimeterRectangle = 2 * (sideA + sideB);
+console.log(perimeterRectangle);
+
+let celsius = 25;
+let fahrenheit = celsius * 9 / 5 + 32;
+console.log(fahrenheit);
+
+fahrenheit = 77;
+celsius = (fahrenheit - 32) * 5 / 9;
+console.log(celsius);
+
+
