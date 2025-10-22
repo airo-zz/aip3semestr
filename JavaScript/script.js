ee9ccd0ab80e9ffe8a95a67e2470dac7bc61bfda
Emir Dzhalalov <dzalalovemran@gmail.com>
2025-10-22 16:43:57 +0300

    v57

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let num1 = prompt('Введите первое число');
-let num2 = prompt('Введите второе число');
-
-alert(Number(num1) + Number(num2));
+let sideA = parseFloat(prompt("Введите длину первой стороны прямоугольника:"));
+let sideB = parseFloat(prompt("Введите длину второй стороны прямоугольника:"));
+let perimeter = 2 * (sideA + sideB);
+alert("Периметр прямоугольника: " + perimeter);
