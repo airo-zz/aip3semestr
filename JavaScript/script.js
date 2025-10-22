e0082c867ea669dcbf89114e0f4576fef69985f5
Emir Dzhalalov <dzalalovemran@gmail.com>
2025-10-22 17:35:55 +0300

    v72

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,20 @@
 "use strict";
 
-let arr = ['a', 'b', 'c', 'd', 'e'];
-delete arr[1];
-delete arr[2];
-console.log(arr.length);
+const daysOfWeek = {
+    1: "Понедельник",
+    2: "Вторник",
+    3: "Среда",
+    4: "Четверг",
+    5: "Пятница",
+    6: "Суббота",
+    7: "Воскресенье"
+};
+
+console.log("1: " + daysOfWeek[1]);
+console.log("2: " + daysOfWeek[2]);
+console.log("3: " + daysOfWeek[3]);
+console.log("4: " + daysOfWeek[4]);
+console.log("5: " + daysOfWeek[5]);
+console.log("6: " + daysOfWeek[6]);
+console.log("7: " + daysOfWeek[7]);
+
