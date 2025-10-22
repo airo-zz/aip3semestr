69ab03d73e06e2e41f1f62fc43670d333caf95ae
Emir Dzhalalov <dzalalovemran@gmail.com>
2025-10-22 17:10:09 +0300

    v62

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,30 +1,9 @@
 "use strict";
 
+let numbers = [1, 2, 3, 4, 5];
+alert(numbers);
+console.log(numbers);
 
-let radius = 5;
-let areaCircle = Math.PI * radius * radius;
-console.log(areaCircle);
-
-let sideSquare = 4;
-let areaSquare = sideSquare * sideSquare;
-console.log(areaSquare);
-
-let sideA = 3;
-let sideB = 6;
-let areaRectangle = sideA * sideB;
-console.log(areaRectangle);
-
-sideA = 3;
-sideB = 6;
-let perimeterRectangle = 2 * (sideA + sideB);
-console.log(perimeterRectangle);
-
-let celsius = 25;
-let fahrenheit = celsius * 9 / 5 + 32;
-console.log(fahrenheit);
-
-fahrenheit = 77;
-celsius = (fahrenheit - 32) * 5 / 9;
-console.log(celsius);
-
-
+let strings = ["apple", "banana", "cherry"];
+alert(strings);
+console.log(strings);
