fb013d79337cd08b0bef61d404b1da72fbf6b60f
Emir Dzhalalov <dzalalovemran@gmail.com>
2025-10-22 17:26:50 +0300

    v70

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
-let arr = [1, 2, 3];
-arr.push(4);
-arr.push(5);
-console.log(arr);
+let arr = [1, 2, 3, 4, 5];
+let key1 = 1;
+let key2 = 2;
+let sum = arr[key1] + arr[key2];
+console.log(sum);
