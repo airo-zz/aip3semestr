b8519ed7b13e499547a36d468005adf5a3689d71
Emir Dzhalalov <dzalalovemran@gmail.com>
2025-10-22 17:43:07 +0300

    v74

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,17 +1,8 @@
 "use strict";
 
-let obj = {
-    1: 'Январь',
-    2: 'Февраль',
-    3: 'Март',
-    4: 'Апрель',
-    5: 'Май',
-    6: 'Июнь',
-    7: 'Июль',
-    8: 'Август',
-    9: 'Сентябрь',
-    10: 'Октябрь',
-    11: 'Ноябрь',
-    12: 'Декабрь'
+let user = {
+    name: 'Иван',
+    surname: 'Иванов',
+    patronymic: 'Иванович'
 };
-console.log(obj);
+console.log(user.surname + ' ' + user.name + ' ' + user.patronymic);
