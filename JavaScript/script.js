a9e12e623b9c547a15e49920a11b59c5ac9001b0
Emir Dzhalalov <dzalalovemran@gmail.com>
2025-10-22 17:14:09 +0300

    v65

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,8 +1,8 @@
 "use strict";
 
 
-let arr1 = [5, 'abc', true, null, 12];
-console.log(arr1.length);
-
-let arr2 = ['apple', 42, false, 3.14, 'end'];
-console.log(arr2[arr2.length - 1]);
+let arr = ['a', 'b', 'c'];
+arr[0] = 1;
+arr[1] = 2;
+arr[2] = 3;
+console.log(arr);
